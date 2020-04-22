4f38790180099b6a12392b5632a92c2959df9aa1
sunvirgo <g_sun2java@163.com>
2020-04-22 21:08:34 +0800

    sunvirgo_hg：step03工程初始化

---
+++ src/main/java/com/example/springbootstep03/SpringBootStep03Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootstep03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStep03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStep03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootstep03/SpringBootStep03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstep03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStep03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
